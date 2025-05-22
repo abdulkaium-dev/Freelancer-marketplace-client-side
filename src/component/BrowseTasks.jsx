c78be3ce5916dd48f46d51fd13a3ded82a93642d
Abdulkaium775 <abdulkaium.80775@gmail.com>
2025-05-22 19:58:55 +0600

    13th commit

--- src/component/BrowseTasks.jsx
+++ src/component/BrowseTasks.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const BrowseTasks = () => {
+const browseTasks = () => {
     return (
         <div>
             
@@ -8,4 +8,4 @@ const BrowseTasks = () => {
     );
 };
 
-export default BrowseTasks;
+export default browseTasks;
